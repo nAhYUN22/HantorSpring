81e9ef1bfba144e0038b63b1d76ca9e8137058df
NaHyun22 <nhle0217@ajou.ac.kr>
2024-11-18 05:00:17 +0900

    chap.07

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -50,7 +50,7 @@ public class ArticleController {
         //3. 뷰 페이지 반환하기
         return "articles/show";
     }
-    @GetMapping("/articles/")
+    @GetMapping("/articles")
     public String index(Model model){
         //1. 모든 데이터 가져오기
         List<Article> articleEntityList = articleRepository.findAll();
@@ -59,4 +59,31 @@ public class ArticleController {
         //3. 뷰 페이지 설정하기
         return "articles/index";
     }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+        //수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        //뷰 페이지 설정하기
+        return "articles/edit";
+    }
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form){
+        log.info(form.toString());
+        //1. DTO -> 엔티티로 변환하기
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+        //2. 엔티티를 DB에 저장하기
+        //2-1. DB에서 기존 데이터 가져오기
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        //2-2. 기존 데이터 값을 갱신하기
+        if (target != null){
+            articleRepository.save(articleEntity); //엔티티를 DB에 저장(갱신)
+        }
+        //3. 수정 결과 페이지를 리다이렉트하기
+        return "redirect:/articles/" + articleEntity.getId();
+    }
+
 }
