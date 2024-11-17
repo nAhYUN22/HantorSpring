00458d07791c41472d224386f7a5443dab3d1df9
NaHyun22 <nhle0217@ajou.ac.kr>
2024-11-18 05:33:59 +0900

    chap.08

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
@@ -72,5 +73,15 @@ public class MemberController {
             memberRepository.save(memberEntity);
         }
         return "redirect:/members"+memberEntity.getId();
+    }
+    @GetMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+        log.info("삭제 요청이 들어왔습니다.");
+        Member target = memberRepository.findById(id).orElse(null);
+        if(target != null){
+            memberRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제되었습니다.");
+        }
+        return "redirect:/members";
     }
 }
